fd2299f3eef88cdebdfcbefc6880731fa5d05e88
Nehemiah Mekonnen <binyamm83@gmail.com>
2025-07-26 13:17:53 +0300

    feat: changed id field from int to uuid field

--- alx-project-nexus/user/models.py
+++ alx-project-nexus/user/models.py
@@ -1,14 +1,18 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     full_name = models.CharField(max_length=255, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
 
 
 class Follow(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
     following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
     created_at = models.DateTimeField(auto_now_add=True)
--- alx-project-nexus/user/views.py
+++ alx-project-nexus/user/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from user.models import Follow
+
+
+class FollowerViewSet(ModelViewSet):
+    """
+    A viewset for viewing and editing follower instances.
+    """
+    queryset = Follow.objects.all()
+    serializer_class = FollowerSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Follow.objects.filter(following=user)
