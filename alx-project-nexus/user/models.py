28e80c92e63cff66f7427f43744284447c4dae1e
Nehemiah Mekonnen <binyamm83@gmail.com>
2025-07-27 14:28:56 +0300

    feat: make email unique field in user model, add follow model

--- alx-project-nexus/user/models.py
+++ alx-project-nexus/user/models.py
@@ -10,12 +10,13 @@ class User(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
     email_verified = models.BooleanField(default=False)
+    email = models.EmailField(unique=True, db_index=True)
 
 
 class Follow(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
-    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following', null=True, blank=True)
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
